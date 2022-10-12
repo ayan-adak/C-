856d0c39d47e76b32e002d30684affee580dd488
ayan-adak <101331393+ayan-adak@users.noreply.github.com>
2022-10-12 15:47:55 +0530

    Update 5_6pro.cpp

--- cppexamples/5_6pro.cpp
+++ cppexamples/5_6pro.cpp
@@ -1,46 +1,42 @@
 #include<iostream>
 using namespace std;
-class time
+class employee
 {
-    int hours;
-    int minutes;
+    char name[30];
+    float age;
     public:
-    void gettime(int h, int m)
-    {
-        hours = h;
-        minutes = m;
-    }
-    void puttime()
-    {
-        cout<< hours << " hours and ";
-        cout<< minutes << " minutes "<<endl;
-    }
-
-    void sum(time, time);
+    void getdata();
+    void putdata();
 };
 
-void time :: sum(time t1, time t2)
+void employee :: getdata()
 {
-    minutes = t1.minutes + t2.minutes;
-    hours = minutes/60;
-    minutes = minutes % 60;
-    hours = hours + t1.hours + t2.hours;
+    cout<< "Enter name: ";
+    cin>>name;
+    cout<< "Enter age: ";
+    cin>>age;
 }
 
+void employee :: putdata()
+{
+    cout << "Name: "<<name<<endl;
+    cout<<"Age: "<<age<<endl;
+}
+
+const int size=3;
 int main()
 {
-    time T1,T2,T3;
-    T1.gettime(2,45);
-    T2.gettime(3,30);
-
-    T3.sum(T1,T2);
-
-    cout<<"T1 = ";
-    T1.puttime();
-    cout<<"T2 = ";
-    T2.puttime();
-    cout<<"T3 = ";
-    T3.puttime();
-
+    employee manager[size];
+    for(int i=0 ; i<size ; i++)
+    {
+        cout<<"Details of manager "<<i+1<<endl;
+        manager[i].getdata();
+    }
+    cout<<endl;
+    for(int i=0 ; i<size ; i++)
+    {
+        cout<<"Manager "<<i+1<<endl;
+        manager[i].putdata();
+    }
     return 0;
 }
